73815ce89c1639c921fc8670f855d09744fb5182
Kenny <kenny55660955@gmail.com>
2021-06-27 22:55:56 +0800

    feat: Finish Q1

--- 1.Two Sum.playground/Contents.swift
+++ 1.Two Sum.playground/Contents.swift
@@ -1,29 +1,47 @@
-import UIKit
+import Foundation
+
+// MARK: - Question
 /*
- Question:
  1. Two Sum
- Easy
- 22007
- 746
- Add to List
- Share
  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
  You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
  You can return the answer in any order.
-
+ 
  Example 1:
-
  Input: nums = [2,7,11,15], target = 9
  Output: [0,1]
  Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+ 
  Example 2:
-
  Input: nums = [3,2,4], target = 6
  Output: [1,2]
+ 
  Example 3:
-
  Input: nums = [3,3], target = 6
  Output: [0,1]
  */
+
+// MARK: - Answer
+let nums = [2, 7, 11, 15]
+
+let target = 9
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    
+    var resultDic = [Int: Int]()
+    
+    for (i, num) in nums.enumerated() {
+        if let finalIndex = resultDic[target - num] {
+            return [finalIndex, i]
+        }
+        
+        resultDic[num] = i
+    }
+    return []
+}
+
+let result = twoSum(nums, target)
+
+print(result)
+
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # LeetCode-Swift
 Practice LeetCode by Swift.
+
